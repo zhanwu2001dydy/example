125b030d62ff90b43af32a8498e1aafae2c2e5bf
wuzhan <zhan_wu2001@aliyun.com>
2017-02-18 20:50:22 +0800

    add current frame test

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -7,10 +7,13 @@ public class Game {
     private int score = 0;
     private int[] itsThrows = new int[21];
     private int itsCurrentThrows = 0;
+    private int currentFrame;
+    private boolean isFirstThrow  = true;
 
     public void add(int pins) {
          itsThrows[itsCurrentThrows++] = pins;
         score += pins;
+        adjustCurrentFrame(pins);
     }
 
     public int getScore() {
@@ -25,12 +28,29 @@ public class Game {
             int secondThrow = itsThrows[ball++];
             int frameScore = firstThrow + secondThrow;
             if(frameScore == 10){
-                score += frameScore + itsThrows[ball++];
+                score += frameScore + itsThrows[ball];
             }else {
                 score += frameScore;
             }
-
         }
         return score;
+    }
+
+    private void adjustCurrentFrame(int pins) {
+        if(isFirstThrow){
+            if(pins == 10){
+                currentFrame++;
+            }else {
+                isFirstThrow = false;
+                return;
+            }
+
+        }
+        currentFrame++;
+        isFirstThrow = true;
+    }
+
+    public int getCurrentFrame() {
+        return currentFrame;
     }
 }
--- src/com/company/GameTest.java
+++ src/com/company/GameTest.java
@@ -23,7 +23,9 @@ public class GameTest {
     public void  testTwoThrows(){
         g.add(5);
         g.add(4);
+        assertEquals(1,g.getCurrentFrame());
         assertEquals(9, g.getScore());
+
     }
 
     @Test
@@ -32,6 +34,7 @@ public class GameTest {
         g.add(4);
         g.add(7);
         g.add(2);
+        assertEquals(2,g.getCurrentFrame());
         assertEquals(18,g.getScore());
         assertEquals(9,g.getScoreForFrame(1));
         assertEquals(18,g.getScoreForFrame(2));
@@ -42,6 +45,20 @@ public class GameTest {
         g.add(2);
         g.add(8);
         g.add(3);
+        assertEquals(1,g.getCurrentFrame());
         assertEquals(13,g.getScoreForFrame(1));
     }
+
+    public void testSimpleFrameAfterSpare() throws Exception {
+        g.add(2);
+        g.add(8);
+        g.add(3);
+        g.add(2);
+        assertEquals(2,g.getCurrentFrame());
+        assertEquals(13,g.getScoreForFrame(1));
+        assertEquals(18,g.getScoreForFrame(2));
+        assertEquals(18,g.getScore());
+    }
+
+
 }
