5808e8d027b68032eb9015e18cbad552c13e8a42
wuzhan <zhan_wu2001@aliyun.com>
2017-02-18 20:05:51 +0800

    add two throws test

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -4,12 +4,17 @@ package com.company;
  * Created by wuzhan on 2017/2/18.
  */
 public class Game {
-    private int score = 5;
+    private int score = 0;
 
     public void add(int i) {
+        score += i;
     }
 
     public int getScore() {
         return score;
+    }
+
+    public int getscoreForFrame(int i) {
+        return 0;
     }
 }
--- src/com/company/GameTest.java
+++ src/com/company/GameTest.java
@@ -1,14 +1,40 @@
 package com.company;
 
 import junit.framework.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.Assert.*;
+
 public class GameTest {
+    Game g ;
+    @Before
+    public void setUp() throws Exception {
+        g = new Game();
+
+    }
 
     @Test
     public void  testOneThrows(){
-        Game g = new Game();
         g.add(5);
-        Assert.assertEquals(5, g.getScore());
+        assertEquals(5, g.getScore());
+    }
+
+    @Test
+    public void  testTwoThrows(){
+        g.add(5);
+        g.add(4);
+        assertEquals(9, g.getScore());
+    }
+
+    @Test
+    public void testFourThrowsNoMark(){
+        g.add(5);
+        g.add(4);
+        g.add(7);
+        g.add(2);
+        assertEquals(18,g.getScore());
+        assertEquals(9,g.getscoreForFrame(1));
+        assertEquals(18,g.getscoreForFrame(2));
     }
 }
