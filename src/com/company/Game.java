cff0648faf424834ffafdc6c6e0e4e3d45aecfba
wuzhan <zhan_wu2001@aliyun.com>
2017-02-20 23:30:27 +0800

    refactor 2

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -4,51 +4,23 @@ package com.company;
  * Created by wuzhan on 2017/2/18.
  */
 public class Game {
-    private int score = 0;
-    private int[] itsThrows = new int[21];
-    private int itsCurrentThrows = 0;
     private int currentFrame;
     private boolean isFirstThrow  = true;
-    int ball;
+    private Scorer itsScorer = new Scorer();
 
 
     public void add(int pins) {
-        itsThrows[itsCurrentThrows++] = pins;
-        score += pins;
+        itsScorer.addThrow(pins);
         adjustCurrentFrame(pins);
     }
 
     public int getScore() {
-        return scoreForFrame(getCurrentFrame());
+        return itsScorer.scoreForFrame(getCurrentFrame());
     }
 
     public int scoreForFrame(int theFrame) {
-        int score = 0;
-        ball = 0;
-        for(int currentFrame = 0;currentFrame < theFrame; currentFrame++){
-            if(strike()){
-                score += 10 + nextTwoBallsForSpike();
-                ball += 1;
-            }else if(spare()){
-                score += 10 + nextBallForSpare();
-                ball += 2;
-            }else {
-                score += nextTwoBalls();
-                ball += 2;
-
-            }
-        }
-        return score;
+        return itsScorer.scoreForFrame(theFrame);
     }
-
-    private int nextBallForSpare() {
-        return itsThrows[ball+2];
-    }
-
-    private boolean spare() {
-        return nextTwoBalls() == 10;
-    }
-
 
     private void adjustCurrentFrame(int pins) {
         if(isFirstThrow == true){
@@ -72,15 +44,4 @@ public class Game {
         return currentFrame;
     }
 
-    private int nextTwoBallsForSpike() {
-        return itsThrows[ball+1] + itsThrows[ball + 2];
-    }
-
-    private int nextTwoBalls() {
-        return itsThrows[ball] + itsThrows[ball + 1];
-    }
-
-    private boolean strike() {
-        return itsThrows[ball] == 10;
-    }
 }
